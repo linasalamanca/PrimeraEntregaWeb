c563a90efaffaa41035269a5c311eaa5b0db058f
linasalamanca <118871999+linasalamanca@users.noreply.github.com>
2024-02-29 17:55:16 -0500

    atributos

--- src/main/java/com/example/PrimeraEntregaWeb/model/Jugador.java
+++ src/main/java/com/example/PrimeraEntregaWeb/model/Jugador.java
@@ -1,8 +1,23 @@
 package com.example.PrimeraEntregaWeb.model;
 
+import javax.validation.constraints.NotBlank;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
 public class Jugador {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "rol", nullable = false)
+    @NotBlank(message = "no puede estar en blanco")
     private String rol;
+    @Column(name = "usuario", nullable = false)
+    @NotBlank(message = "no puede estar en blanco")
     private String usuario;
+    @Column(name = "contrasena", nullable = false)
+    @NotBlank(message = "no puede estar en blanco")
     private String contrasena;
 
     public String getRol() {
--- src/main/java/com/example/PrimeraEntregaWeb/model/Producto.java
+++ src/main/java/com/example/PrimeraEntregaWeb/model/Producto.java
@@ -1,7 +1,19 @@
 package com.example.PrimeraEntregaWeb.model;
 
+import javax.validation.constraints.NotBlank;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
 public class Producto {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "volumen", nullable = false)
+    @NotBlank(message = "no puede estar en blanco")
     private Float volumen;
+    @Column(name = "tipo", nullable = false)
+    @NotBlank(message = "no puede estar en blanco")
     private String tipo;
 
     public Float getVolumen() {
